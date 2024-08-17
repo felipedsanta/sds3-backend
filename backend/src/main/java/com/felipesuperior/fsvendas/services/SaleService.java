ab96f6f09a6064f17b40c3d43b41b1caf136ff5b
Felipe <felipe.santos@liga.ventures>
2024-08-17 01:43:20 -0300

    Homolog

---
+++ backend/src/main/java/com/felipesuperior/fsvendas/services/SaleService.java
@@ -0,0 +1,44 @@
+package com.felipesuperior.fsvendas.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.felipesuperior.fsvendas.dto.SaleDTO;
+import com.felipesuperior.fsvendas.dto.SaleSuccessDTO;
+import com.felipesuperior.fsvendas.dto.SaleSumDTO;
+import com.felipesuperior.fsvendas.entities.Sale;
+import com.felipesuperior.fsvendas.repositories.SaleRepository;
+import com.felipesuperior.fsvendas.repositories.SellerRepository;
+
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}
+}
